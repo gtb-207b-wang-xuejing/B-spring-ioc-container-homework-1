6231dcfca55b629abaf4457532cbe49cd1e25f59
WANG961007 <mw1023@connect.hku.hk>
2021-11-09 15:21:21 +0800

    [wangxuejing]the first answer of Problem 2

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,11 +1,25 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 
 @Service
-public class GreetingService {
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE,
+        proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class GreetingService implements ApplicationContextAware {
 
     String sayHi() {
         return "hello world";
+    }
+
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println(applicationContext.getBean(GreetingRepository.class));
     }
 }
